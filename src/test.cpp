d4a0f8d23a7cba215559cf014511a8cab7ff63ce
jiangcl <1436316764@qq.com>
2024-10-25 23:03:33 +0800

    version 1.0

--- src/test.cpp
+++ src/test.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "min.h"
+#include "add.h"
+
 using namespace std;
 
 int main(){
   int a,b;
   cin>>a>>b;
   cout<<"the min is : "<<min(a,b)<<endl;
+  cout<<"the sum is : "<<add(a,b)<<endl;
   return 0;  
 }
